921c0f4c03543da4a4ddc6213fe7305e35cebf26
Egor Vinogradov <egorvinogradov@live.ru>
2015-04-13 14:43:17 -0700

    classes form & list

--- registration-system.js
+++ registration-system.js
@@ -10,26 +10,82 @@ if (Meteor.isClient) {
 
   Template.list.helpers({
     classes: function () {
-      return [
-        {
-          name: 'BA 360',
-          title: 'Management Information Systems'
-        },
-        {
-          name: 'MATH 15',
-          title: 'Finite Mathematics'
-        },
-        {
-          name: 'BA 300B',
-          title: 'Financial Accounting Foundations'
-        },
-        {
-          name: 'BA 301 I',
-          title: 'Managerial Economics'
-        }
-      ]
+      var classes;
+      try {
+        classes = Meteor.user().profile.classes;
+      }
+      catch (e) {}
+      return classes;
     }
   });
+
+  Template.list.events({
+    'click .drop-class': function(){
+      if ( confirm('Are you sure you want to drop ' + this.abbr + ' ' + this.title) ) {
+        var updatedClasses = Meteor.user().profile.classes.filter(function(item){
+          return item.abbr !== this.abbr;
+        }, this);
+
+        Meteor.users.update({
+          _id: Meteor.user()._id
+        }, {
+          $set: { 'profile.classes': updatedClasses }
+        });
+      }
+    }
+  });
+
+  Template.form.events({
+    'submit form': function(e){
+
+      var abbrInput = $('#class-abbr');
+      var titleInput = $('#class-title');
+
+      var abbr = abbrInput.val().trim();
+      var title = titleInput.val().trim();
+
+      if ( abbr && title ) {
+
+        var classes = Meteor.user().profile.classes || [];
+        var classIndex;
+
+        for ( var i = 0; i < classes.length; i++ ) {
+          if ( classes[i].abbr === abbr ) {
+            classIndex = i;
+            break;
+          }
+        }
+
+        var newValue = {
+            abbr: abbr,
+            title: title
+        };
+
+        if (classIndex > -1) {
+          if (confirm('This class is already added. Do you wand to update it?')) {
+            classes[classIndex] = newValue;
+          }
+        }
+        else {
+          classes.push(newValue);
+        }
+
+        Meteor.users.update({
+          _id: Meteor.user()._id
+        }, {
+          $set: { 'profile.classes': classes }
+        });
+
+      }
+
+      abbrInput.val('');
+      titleInput.val('');
+
+      e.preventDefault();
+
+    }
+  });
+
 
 
 
@@ -54,5 +110,15 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+
+
+  Meteor.users.allow({
+    'insert': function (userId,doc) {
+      /* user and doc checks ,
+      return true to allow insert */
+      return true;
+    }
+  });
+
   });
 }
